da2292d328d7780cb6afe281def86fb3238ebc1a
Shubham Kumar <ishubhamjnv@gmail.com>
2024-04-30 08:06:48 +0530

    create header and footer and initial loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <>
-      <div className="text-red-500">Blog App</div>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="flex flex-wrap content-between min-h-screen">
+      <div className="block w-full">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
