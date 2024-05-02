e11624ab4ce1f3644544808a3fff37f11f79a48d
Shubham Kumar <ishubhamjnv@gmail.com>
2024-05-02 21:52:56 +0530

    create Login and Signup page

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,9 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "../components/Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
+import Select from "./Select";
 
-export { Header, Footer, Container, Logo };
+export { Header, Footer, Container, Logo, LogoutBtn, Input, Button, Select };
