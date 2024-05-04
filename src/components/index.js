7f5b32d0d17f33f873db50ecea6fd2655ab43dc5
Shubham Kumar <ishubhamjnv@gmail.com>
2024-05-04 23:54:25 +0530

    create Post-Form and Login, Signup, Addpost, Allposts, Editpost and home pages

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,26 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
 import Button from "./Button";
 import Select from "./Select";
+import RTE from "./RTE";
+import AuthLayout from "./AuthLayout";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Input, Button, Select };
+export {
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  Button,
+  Select,
+  RTE,
+  AuthLayout,
+};
